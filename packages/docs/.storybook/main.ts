dcfa88ab2537e97a25cbb17d3c4a04d44bcb0519
Wallace W <wallace_wolv@hotmail.com>
2025-04-06 15:47:24 -0300

    feat: Story: Button

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -6,13 +6,13 @@ import { join, dirname } from "path"
 * This function is used to resolve the absolute path of a package.
 * It is needed in projects that use Yarn PnP or are set up within a monorepo.
 */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string){
   return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
   ],
   "addons": [
     {
--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vite/client" />
+/// <reference types='vite/client' />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
